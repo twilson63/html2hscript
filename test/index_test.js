eee7ea9741ce9a8556ee0aa221141e107a6d213a
Tom Wilson <tom@jackhq.com>
2015-03-14 11:15:02 -0400

    fixed attributes test

--- test/index_test.js
+++ test/index_test.js
@@ -2,8 +2,8 @@ var test = require('tap').test;
 var parser = require('../');
 
 test('should return hyperscript', function(t) {
-  parser('<h1>Hello World</h1>', function(err, hscript) {
-    t.equals(hscript, 'h("h1", [ "Hello World" ])', 'success')
+  parser('<h1 foo="beep">Hello World</h1>', function(err, hscript) {
+    t.equals(hscript, 'h("h1", { "foo": "beep"}, [ "Hello World" ])', 'success')
     t.end()
   })
 })
